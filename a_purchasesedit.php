3dfe222c0d2e306df10840a8e3d013517e082e38
Emmanuel Okum <emmanuel.okum@idea2solutions.com>
2024-01-16 13:30:26 +0100

    removed all folder

--- a_purchasesedit.php
+++ a_purchasesedit.php
@@ -267,11 +267,11 @@ class ca_purchases_edit extends ca_purchases {
 
 		// Page ID
 		if (!defined("EW_PAGE_ID"))
-			define("EW_PAGE_ID", 'edit', TRUE);
+			define("EW_PAGE_ID", 'edit');
 
 		// Table name (for backward compatibility)
 		if (!defined("EW_TABLE_NAME"))
-			define("EW_TABLE_NAME", 'a_purchases', TRUE);
+			define("EW_TABLE_NAME", 'a_purchases');
 
 		// Start timer
 		if (!isset($GLOBALS["gTimer"])) $GLOBALS["gTimer"] = new cTimer();
